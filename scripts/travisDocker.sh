a5692f2cb633555854806e06bd42ed824a3b96dc
Magnus Groß <magnus.gross@rwth-aachen.de>
2018-02-27 02:33:10 +0100

    Fix travis

--- scripts/travisDocker.sh
+++ scripts/travisDocker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -t --rm=true base/archlinux /bin/sh -c "pacman -Syu --noconfirm make gcc git qt5-base qt5-declarative qt5-svg qt5-quickcontrols2 qt5-graphicaleffects && git clone --recursive https://github.com/magnus-gross/quickcurver.git && cd quickcurver && scripts/build.sh"
+docker run -t --rm=true base/archlinux /bin/sh -c "pacman -Syu --noconfirm make gcc git qt5-base qt5-declarative qt5-svg qt5-quickcontrols2 qt5-graphicaleffects && git clone --recursive https://github.com/magnus-gross/quickcurver.git && git checkout develop && cd quickcurver && scripts/build.sh"
