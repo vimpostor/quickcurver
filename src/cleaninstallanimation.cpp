vimpostor/quickcurver

commit 754bd9758bc1a0d2b060a8b341c0edc5649ca683

Improve cleaninstallanimation

src/cleaninstallanimation.cpp
@@ -3,15 +3,6 @@
 #define ANIMATION_DURATION 300
 
 /**
- * @brief Creates a CleaninstallAnimation object
- * @param parent The parent object
- */
-CleaninstallAnimation::CleaninstallAnimation(QObject *parent) : QObject(parent)
-{
-	connect(&timer, &QTimer::timeout, this, &CleaninstallAnimation::progress);
-}
-
-/**
  * @brief Triggers the cleaninstall animation
  * @param newSegments The current state of segments in the Curver object
  *
@@ -24,7 +15,7 @@ void CleaninstallAnimation::trigger(std::vector<std::unique_ptr<Segment> > &newS
 		// do not spawn an animation, if there is nothing to animate
 		return;
 	}
-	timer.stop();
+	active = false;
 	for (auto it = newSegments.begin(); it != newSegments.end(); ++it) {
 		segments.push_back(std::move(*it));
 	}
@@ -38,7 +29,7 @@ void CleaninstallAnimation::trigger(std::vector<std::unique_ptr<Segment> > &newS
 	}
 	totalSize = Util::accumulate(sizeCache, 0);
 	initialTime = QTime::currentTime();
-	timer.start(16);
+	active = true;
 }
 
 /**
@@ -46,10 +37,14 @@ void CleaninstallAnimation::trigger(std::vector<std::unique_ptr<Segment> > &newS
  */
 void CleaninstallAnimation::progress()
 {
+	if (!active) {
+		return;
+	}
 	const float timeSinceStart = initialTime.msecsTo(QTime::currentTime());
 	const float factor = timeSinceStart / ANIMATION_DURATION;
-	const size_t pos = totalSize * factor;
-	if (factor < 1) {
+	const float easedFactor = Util::easeInOutSine(factor);
+	const size_t pos = totalSize * easedFactor;
+	if (easedFactor < 1) {
 		size_t pointsToDelete = pos;
 		size_t segmentIndex;
 		for (segmentIndex = 0; segmentIndex < sizeCache.size() && pointsToDelete >= sizeCache[segmentIndex]; ++segmentIndex) {
@@ -60,7 +55,7 @@ void CleaninstallAnimation::progress()
 		segments[segmentIndex]->popPoints(pointsToDelete - pointsDeleted[segmentIndex]);
 		pointsDeleted[segmentIndex] = pointsToDelete;
 	} else {
-		timer.stop();
+		active = false;
 		segments.clear();
 	}
 }

src/cleaninstallanimation.h
@@ -20,13 +20,9 @@ class CleaninstallAnimation : public QObject
 {
 	Q_OBJECT
 public:
-	explicit CleaninstallAnimation(QObject *parent = nullptr);
 	void trigger(std::vector<std::unique_ptr<Segment>>& newSegments);
+	void progress();
 private:
-	/**
-	 * @brief The timer updating the animation
-	 */
-	QTimer timer;
 	/**
 	 * @brief The point in time that the animation was triggered with trigger()
 	 */
@@ -49,8 +45,10 @@ private:
 	 * @brief The total size over all segments
 	 */
 	size_t totalSize;
-private slots:
-	void progress();
+	/**
+	 * @brief Whether the animation is currently running
+	 */
+	bool active = false;
 };
 
 #endif // CLEANINSTALLANIMATION_H

src/curver.cpp
@@ -331,6 +331,8 @@ void Curver::progress(int deltat, std::vector<std::unique_ptr<Curver> > &curvers
 			die();
 		}
 	}
+
+	cleaninstallAnimation.progress();
 }
 
 /**

src/util.cpp
@@ -1,5 +1,7 @@
 #include "util.h"
 
+#include <math.h>
+
 /**
  * @brief Initializes the random seed
  */
@@ -113,4 +115,9 @@ QQuickView* Util::getTextureGenerator()
 qint64 Util::getTimeDiff(const QTime& t)
 {
 	return t.msecsTo(QTime::currentTime());
+}
+
+float Util::easeInOutSine(const float& a)
+{
+	return 0.5 * (1 - std::cos(M_PI * a));
 }

src/util.h
@@ -50,6 +50,7 @@ namespace Util {
 	void setBit(uint8_t& byte, const int pos, bool value);
 	QQuickView* getTextureGenerator();
 	qint64 getTimeDiff(const QTime& t);
+	float easeInOutSine(const float& a);
 
 	// std algorithm wrappers
 

commit f4241583b239769d5f6dc332699e5ec7623d27da

Use a separate timer for CleaninstallAnimation
The other design does not work well with online multiplayer.

Regression was introduced in 754bd97

src/cleaninstallanimation.cpp
@@ -3,6 +3,15 @@
 #define ANIMATION_DURATION 300
 
 /**
+ * @brief Creates a CleaninstallAnimation object
+ * @param parent The parent object
+ */
+CleaninstallAnimation::CleaninstallAnimation(QObject* parent)
+{
+	connect(&timer, &QTimer::timeout, this, &CleaninstallAnimation::progress);
+}
+
+/**
  * @brief Triggers the cleaninstall animation
  * @param newSegments The current state of segments in the Curver object
  *
@@ -15,7 +24,7 @@ void CleaninstallAnimation::trigger(std::vector<std::unique_ptr<Segment> > &newS
 		// do not spawn an animation, if there is nothing to animate
 		return;
 	}
-	active = false;
+	timer.stop();
 	for (auto it = newSegments.begin(); it != newSegments.end(); ++it) {
 		segments.push_back(std::move(*it));
 	}
@@ -29,7 +38,7 @@ void CleaninstallAnimation::trigger(std::vector<std::unique_ptr<Segment> > &newS
 	}
 	totalSize = Util::accumulate(sizeCache, 0);
 	initialTime = QTime::currentTime();
-	active = true;
+	timer.start(16);
 }
 
 /**
@@ -37,9 +46,6 @@ void CleaninstallAnimation::trigger(std::vector<std::unique_ptr<Segment> > &newS
  */
 void CleaninstallAnimation::progress()
 {
-	if (!active) {
-		return;
-	}
 	const float timeSinceStart = initialTime.msecsTo(QTime::currentTime());
 	const float factor = timeSinceStart / ANIMATION_DURATION;
 	const float easedFactor = Util::easeInOutSine(factor);
@@ -55,7 +61,7 @@ void CleaninstallAnimation::progress()
 		segments[segmentIndex]->popPoints(pointsToDelete - pointsDeleted[segmentIndex]);
 		pointsDeleted[segmentIndex] = pointsToDelete;
 	} else {
-		active = false;
+		timer.stop();
 		segments.clear();
 	}
 }

src/cleaninstallanimation.h
@@ -20,9 +20,13 @@ class CleaninstallAnimation : public QObject
 {
 	Q_OBJECT
 public:
+	explicit CleaninstallAnimation(QObject *parent = nullptr);
 	void trigger(std::vector<std::unique_ptr<Segment>>& newSegments);
-	void progress();
 private:
+	/**
+	 * @brief The timer updating the animation
+	 */
+	QTimer timer;
 	/**
 	 * @brief The point in time that the animation was triggered with trigger()
 	 */
@@ -45,10 +49,8 @@ private:
 	 * @brief The total size over all segments
 	 */
 	size_t totalSize;
-	/**
-	 * @brief Whether the animation is currently running
-	 */
-	bool active = false;
+private slots:
+	void progress();
 };
 
 #endif // CLEANINSTALLANIMATION_H

src/curver.cpp
@@ -292,16 +292,6 @@ void Curver::prepareSegmentEvent(bool changingSegment, int lower, int upper)
 }
 
 /**
- * @brief Updates the cleaninstall animation
- *
- * This exists to synchronize the animation with the main game timer
- */
-void Curver::updateCleaninstallAnimation()
-{
-	cleaninstallAnimation.progress();
-}
-
-/**
  * @brief Updates the Curver assuming that \a deltat has gone by
  * @param deltat The amount of time since the last update in milliseconds
  * @param curvers All curvers

src/curver.h
@@ -62,7 +62,6 @@ public:
 	bool isAlive() const;
 	void appendPoint(const QPointF pos, const bool changingSegment);
 	void prepareSegmentEvent(bool changingSegment, int lower, int upper);
-	void updateCleaninstallAnimation();
 
 	/**
 	 * @brief The username of the Curver

src/game.cpp
@@ -137,7 +137,6 @@ void Game::progress()
 	int deltat = Util::getTimeDiff(lastProgressTime);
 	lastProgressTime = QTime::currentTime();
 	for (auto &c : getCurvers()) {
-		c->updateCleaninstallAnimation();
 		if (c->isAlive()) {
 			if (c->controller == Curver::Controller::CONTROLLER_BOT) {
 				Bot::makeMove(*c.get());