vimpostor/quickcurver

commit 6923c36053a5bcf1dffb976560ea8271c278f82d

Use the new ColorDialog from Qt 6.4
This also means we don't need Qt.labs anymore.

src/qml/main.qml
@@ -1,6 +1,5 @@
 import QtQuick
 import QtQuick.Window
-import Qt.labs.platform as Platform
 import QtQuick.Controls
 import QtQuick.Controls.Material
 import QtQuick.Layouts
@@ -214,11 +213,11 @@ ApplicationWindow {
 				}
 				ToolButton {
 					icon.source: "qrc:///editor/format_color_fill"
-					icon.color: clientColorDialog.color
+					icon.color: clientColorDialog.selectedColor
 					onClicked: clientColorDialog.open();
-					Platform.ColorDialog {
+					ColorDialog {
 						id: clientColorDialog
-						color: Material.accent
+						selectedColor: Material.accent
 					}
 				}
 				TextField {

CMakeLists.txt
@@ -6,7 +6,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 list(APPEND QT_MODULES Core Qml Quick QuickControls2 Svg)
-find_package(Qt6 6.3 COMPONENTS ${QT_MODULES} REQUIRED)
+find_package(Qt6 6.4 COMPONENTS ${QT_MODULES} REQUIRED)
 set(QT_PREFIXED_MODULES ${QT_MODULES})
 list(TRANSFORM QT_PREFIXED_MODULES PREPEND "Qt6::")
 set(CMAKE_AUTOMOC ON)

src/qml/Players.qml
@@ -1,5 +1,5 @@
-import Qt.labs.platform
 import QtQuick
+import QtQuick.Dialogs
 import QtQuick.Layouts
 import QtQuick.Controls
 import QtQuick.Controls.Material
@@ -120,7 +120,7 @@ Item {
 		}
 		ColorDialog {
 			id: colorDialog
-			onAccepted: c_playerModel.setColor(playerListView.modelIndex, color);
+			onAccepted: c_playerModel.setColor(playerListView.modelIndex, selectedColor);
 		}
 		Dialog {
 			id: botDialog

commit 55e1935c895d463a7c164b4863755a42cee645ad

Fix ColorDialog color selection
Regression was introduced in 6923c36053a5bcf1dffb976560ea8271c278f82d

src/qml/main.qml
@@ -234,7 +234,7 @@ ApplicationWindow {
 					anchors.right: parent.right
 					onClicked: {
 						c_settings.setClientName(nameTextField.text);
-						c_settings.setClientColor(clientColorDialog.color);
+						c_settings.setClientColor(clientColorDialog.selectedColor);
 						game.connectToHost(ipTextField.text, portTextField.text);
 					}
 				}