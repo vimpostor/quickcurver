b008edbc06d5119fb4d8dea31c8a0d3a959732b3
Magnus Groß <magnus@mggross.com>
2024-01-13 00:04:44 +0100

    Remove useless flake-utils

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
 	description = "Modern Qt/C++ implementation of Achtung die Kurve with online multiplayer";
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
 	};
 
-	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+	outputs = { self, nixpkgs }: {
+		packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
 		let
 			pkgs = nixpkgs.legacyPackages.${system};
 			quartz = pkgs.fetchFromGitHub {
@@ -15,30 +15,26 @@
 				hash = "sha256-sbOoYO87NRF3J3FOnCAM40BtXPtdfyYPPfiNgK3q56o=";
 			};
 		in {
-			packages = rec {
-				default = quickcurver;
+			default = pkgs.stdenv.mkDerivation {
+				pname = "quickcurver";
+				version = "0.1";
 
-				quickcurver = pkgs.stdenv.mkDerivation {
-					pname = "quickcurver";
-					version = "0.1";
+				src = ./.;
 
-					src = ./.;
-
-					nativeBuildInputs = with pkgs; [
-						cmake
-						pkg-config
-						qt6.wrapQtAppsHook
-						imagemagick
-					];
-					buildInputs = with pkgs; [
-						qt6.qtbase
-						qt6.qtdeclarative
-						qt6.qtsvg
-					];
-					cmakeFlags = [("-DFETCHCONTENT_SOURCE_DIR_QUARTZ=" + quartz)];
-					postBuild = "make linux-desktop-integration";
-				 };
-			};
-		}
-	);
+				nativeBuildInputs = with pkgs; [
+					cmake
+					pkg-config
+					qt6.wrapQtAppsHook
+					imagemagick
+				];
+				buildInputs = with pkgs; [
+					qt6.qtbase
+					qt6.qtdeclarative
+					qt6.qtsvg
+				];
+				cmakeFlags = [("-DFETCHCONTENT_SOURCE_DIR_QUARTZ=" + quartz)];
+				postBuild = "make linux-desktop-integration";
+			 };
+		});
+	};
 }
