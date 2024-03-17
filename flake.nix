a5c6c6a63ac848d8a7d44b8c42d055bcf84b63d1
Magnus Groß <magnus@mggross.com>
2024-03-17 16:06:04 +0100

    nix: Deduplicate eachSystem
    Also bump quartz version.

--- flake.nix
+++ flake.nix
@@ -4,37 +4,41 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs }: {
-		packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+	outputs = { self, nixpkgs }:
+	let eachSystem = with nixpkgs.lib; f: foldAttrs mergeAttrs {} (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems.flakeExposed);
+	in eachSystem (system:
 		let
 			pkgs = nixpkgs.legacyPackages.${system};
 			quartz = pkgs.fetchFromGitHub {
 				owner = "vimpostor";
 				repo = "quartz";
 				rev = builtins.head (builtins.match ".*FetchContent_Declare\\(.*GIT_TAG ([[:alnum:]\\.]+).*" (builtins.readFile ./CMakeLists.txt));
-				hash = "sha256-sbOoYO87NRF3J3FOnCAM40BtXPtdfyYPPfiNgK3q56o=";
+				hash = "sha256-gni+5kQkjU1LhoEK6/yVC0x6K36Ra30xM47w893nGJ4=";
 			};
-		in {
-			default = pkgs.stdenv.mkDerivation {
-				pname = "quickcurver";
-				version = "0.1";
+		in
+		{
+			packages = {
+				default = pkgs.stdenv.mkDerivation {
+					pname = "quickcurver";
+					version = "0.1";
 
-				src = ./.;
+					src = ./.;
 
-				nativeBuildInputs = with pkgs; [
-					cmake
-					pkg-config
-					qt6.wrapQtAppsHook
-					imagemagick
-				];
-				buildInputs = with pkgs; [
-					qt6.qtbase
-					qt6.qtdeclarative
-					qt6.qtsvg
-				];
-				cmakeFlags = [("-DFETCHCONTENT_SOURCE_DIR_QUARTZ=" + quartz)];
-				postBuild = "make linux-desktop-integration";
-			 };
-		});
-	};
+					nativeBuildInputs = with pkgs; [
+						cmake
+						pkg-config
+						qt6.wrapQtAppsHook
+						imagemagick
+					];
+					buildInputs = with pkgs; [
+						qt6.qtbase
+						qt6.qtdeclarative
+						qt6.qtsvg
+					];
+					cmakeFlags = [("-DFETCHCONTENT_SOURCE_DIR_QUARTZ=" + quartz)];
+					postBuild = "make linux-desktop-integration";
+				 };
+			};
+		}
+	);
 }
